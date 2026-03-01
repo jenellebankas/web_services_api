9fb55b2fe58a7d6d1ac03734d7050b342afca476
leysban023 <ban023@theleys.net>
2026-03-01 20:35:04 +0000

    removing .devcontainer do not need

--- app/api/v1/routers/flights.py
+++ app/api/v1/routers/flights.py
@@ -8,6 +8,7 @@ from app.schemas import FlightRead, FlightCreate, FlightUpdate
 
 router = APIRouter()
 
+
 @router.post("/", response_model=FlightRead, status_code=201)
 def create_flight(flight: FlightCreate, db: Session = Depends(get_db)):
     service = FlightService(db)
@@ -15,6 +16,7 @@ def create_flight(flight: FlightCreate, db: Session = Depends(get_db)):
         return service.create_flight(flight)
     except ValueError:
         raise HTTPException(status_code=404, detail="Flight creation failed")
+
 
 @router.get("/{flight_id}", response_model=FlightRead)
 def get_flight(flight_id: int, db: Session = Depends(get_db)):
@@ -24,6 +26,7 @@ def get_flight(flight_id: int, db: Session = Depends(get_db)):
     except ValueError:
         raise HTTPException(status_code=404, detail="Flight not found")
 
+
 @router.put("/{flight_id}", response_model=FlightRead)
 def update_flight(flight_id: int, flight_update: FlightUpdate, db: Session = Depends(get_db)):
     service = FlightService(db)
@@ -31,6 +34,7 @@ def update_flight(flight_id: int, flight_update: FlightUpdate, db: Session = Dep
         return service.update_flight(flight_id, flight_update)
     except ValueError:
         raise HTTPException(status_code=404, detail="Flight not found")
+
 
 @router.delete("/{flight_id}", status_code=204)
 def delete_flight(flight_id: int, db: Session = Depends(get_db)):
@@ -40,6 +44,7 @@ def delete_flight(flight_id: int, db: Session = Depends(get_db)):
         return None
     except ValueError:
         raise HTTPException(status_code=404, detail="Flight not found")
+
 
 @router.get("/", response_model=List[FlightRead])
 def read_flights(
@@ -51,6 +56,7 @@ def read_flights(
 ):
     service = FlightService(db)
     return service.get_flights(origin, dest, limit, offset)
+
 
 @router.get("/stats/{airport}")
 def airport_stats(airport: str, db: Session = Depends(get_db)):
